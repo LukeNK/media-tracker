105535de851e4ac89b92b338021ce4083217fb0d
Luke Nguyen Khanh <lukenguyenkhanh@gmail.com>
2026-03-13 10:08:10 -0700

    Import database

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 import express from 'express';
+import fs from 'fs';
 
 const app = express();
 
+// Basic routes that should be available without the server
+// These routes will be available as static files in GitHub Pages
 app.use('/static', express.static('static'));
 
 app.get('/', (req, res) => {
     res.sendFile('index.html', { root: '.' });
 });
 
+app.get('/database', (req, res) => {
+    res.sendFile('database.json', { root: '.' });
+});
+
+// API routes for the server
 app.get('/api/ping', (req, res) => {
-    res.json({ message: 'pong' });
+    res.send('pong');
 });
 
 app.listen(80, () => {
